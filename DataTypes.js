b5864725ef966022643bdd2234430ef07595fb8f
tahirbaba <161713622+tahirbaba@users.noreply.github.com>
2024-03-31 22:59:56 +0500

    Data Type Practice

---
+++ DataTypes.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+// Data Types
+let userName = "Muhammad Tahir Hasni"; //String
+let age = 31; // Number
+let isstudent = true; // Boolean
+let randomValue = "https://www.linkedin.com/in/muhammad-tahir-hasni-021a562ba/"; // Any
+console.log(userName);
+console.log(age);
+console.log(isstudent);
+console.log(randomValue);
+export {};
---
+++ DataTypes.ts
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// Data Types
+
+let userName: string = "Muhammad Tahir Hasni";//String
+let age: number = 31   // Number
+let isstudent: boolean = true   // Boolean
+let randomValue: any = "https://www.linkedin.com/in/muhammad-tahir-hasni-021a562ba/" // Any
+console.log(userName);
+console.log(age);
+console.log(isstudent);
+console.log(randomValue);
